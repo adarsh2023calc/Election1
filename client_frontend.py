98160578a9ac95f347da07c88c457efbce799b8a
adarsh2023calc <adarshs2023@gmail.com>
2025-05-26 21:11:26 +0530

    Removed post request to aqmp

--- client_frontend.py
+++ client_frontend.py
@@ -1,5 +1,12 @@
 from flask import Blueprint, render_template, request
 import requests
+import os
+import pika
+import json
+from dotenv import load_dotenv
+
+
+load_dotenv(".env.local")
 
 client_bp = Blueprint('client', __name__, template_folder='templates')
 
@@ -7,10 +14,10 @@ client_bp = Blueprint('client', __name__, template_folder='templates')
 def vote_form():
     return render_template('vote_form.html')
 
-@client_bp.route('client/submit_vote', methods=['POST'])
+@client_bp.route('/submit_vote', methods=['POST'])
 def submit_vote():
-    voter_id = request.form['voter_id']
-    candidate = request.form['candidate']
+    voter_id = request.form.get('voter_id')
+    candidate = request.form.get('candidate')
 
     payload = {
         'voter_id': voter_id,
@@ -18,11 +25,33 @@ def submit_vote():
     }
 
     try:
-        # Change this to your deployed votingapi URL
-        response = requests.post('http://127.0.0.1:5001/vote', json=payload)
-        if response.status_code == 200:
-            return f"<h3>Vote submitted successfully for {candidate}!</h3><a href='/client'>Vote Again</a>"
+        status = vote(payload)
+        if status == 200:
+            return f"<h3>Vote submitted successfully!</h3><a href='/client'>Vote Again</a>"
         else:
-            return f"<h3>Error: {response.text}</h3><a href='/client'>Try Again</a>"
+            return f"<h3>Error while submitting vote.</h3><a href='/client'>Try Again</a>"
     except Exception as e:
         return f"<h3>Server error: {e}</h3>"
+
+def vote(data):
+    try:
+        url = os.environ["CLOUDAMQP_URL"]
+        if not url:
+            raise ValueError("CLOUDAMQP_URL environment variable not set")
+
+        params = pika.URLParameters(url)
+        connection = pika.BlockingConnection(params)
+        channel = connection.channel()
+        channel.queue_declare(queue='vote_queue', durable=True)
+
+        channel.basic_publish(
+            exchange='',
+            routing_key='vote_queue',
+            body=json.dumps(data),
+            properties=pika.BasicProperties(delivery_mode=2)  # make message persistent
+        )
+        connection.close()
+        return 200
+    except Exception as e:
+        print(f"❌ RabbitMQ error: {e}")
+        return 0
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask
 from admin_frontend import admin_bp
 from client_frontend import client_bp
 
+
 app = Flask(__name__)
 app.register_blueprint(admin_bp, url_prefix="/admin")
 app.register_blueprint(client_bp, url_prefix="/client")
+
